76803b6eeadf6d8f031e516bf947e22875881429
shalinkpatel <shalin_patel@brown.edu>
2020-11-16 13:16:51 -0500

    Fixed randomizer for feature subsets

--- code/deep_tree.py
+++ code/deep_tree.py
@@ -44,10 +44,10 @@ class Node(nn.Module):
     """
     Main tree class. This represents a deep decision tree with learnable decision boundaries.
     """
-    def __init__(self, features, hidden, depth):
+    def __init__(self, features, hidden, depth, id):
         """
         Init function
-        - features: a dictionary of which features that a splitter has access to. Represents map depth => tensor of feature index
+        - features: a dictionary of which features that a splitter has access to. Represents map id => tensor of feature index
         - hidden: the hidden size of splitter
         - depth: the depth that the tree has left to construct
         """
@@ -61,15 +61,17 @@ class Node(nn.Module):
             nn.Softmax()
         )
 
-        self.subset = features[depth]
+        self.subset = features[id]
         self.best = []
 
         if depth == 1:
             self.left = Leaf()
             self.right = Leaf()
         else:
-            self.left = Node(features, hidden, depth - 1)
-            self.right = Node(features, hidden, depth - 1)
+            id += 1
+            self.left = Node(features, hidden, depth - 1, id)
+            id += 1
+            self.right = Node(features, hidden, depth - 1, id)
 
     def populate_best(self, x, y):
         """
@@ -149,12 +151,13 @@ if __name__ == '__main__':
 
     # Subset map. Will be randomized when we use the RF
     features = {
+        3: th.tensor([1, 2]),
         2: th.tensor([0, 2]),
         1: th.tensor([0, 1])
     }
     
     # Construct model
-    model = Node(features, 5, 2)
+    model = Node(features, 5, 2, 1)
     model.populate_best(x, y)
     print(model.best)
 
