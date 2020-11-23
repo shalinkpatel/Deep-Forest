5a79256549a610331866f1e542901bfde9c62ac6
commoncenters <kyran_flynn@brown.edu>
2020-11-23 13:55:47 -0500

    update on preprocess for iris

---
+++ code/iris/preprocess.py
@@ -0,0 +1,13 @@
+import torch as th
+import numpy as np
+
+def get_data(train_file, test_file):
+    """
+    Read and parse the train and test file line by line, then tokenize the sentences to build the train and test data separately.
+    Create a vocabulary dictionary that maps all the unique tokens from your train and test data as keys to a unique integer value.
+    Then vectorize your train and test data based on your vocabulary dictionary.
+
+    :param train_file: Path to the training file.
+    :param test_file: Path to the test file.
+    :return: Tuple of train (1-d list or array with training words in vectorized/id form), test (1-d list or array with testing words in vectorized/id form), vocabulary (Dict containg index->word mapping)
+    """
