02e3c63d6bb822b3fb11e72f1e18b5fb0f8699aa
hannahmanuela <hannahmngross@gmail.com>
2020-11-27 18:25:05 -0500

    added ipynb

--- code/wine/preprocess.py
+++ code/wine/preprocess.py
@@ -1,8 +1,9 @@
 import numpy as np
 from sklearn import datasets
+import torch as th
 
 
-def get_wine_data(pct_train):
+def get_data(pct_train):
     """
     :param pct_train: what percentage of the data should be used to train on
     :return: as numpy arrays: train_data, train_labels, test_data, test_labels
@@ -19,7 +20,12 @@ def get_wine_data(pct_train):
     num_examples = data.shape[0]
     num_train_examples = int(num_examples*pct_train/100)
 
-    return data[:num_train_examples], labels[:num_train_examples], data[num_train_examples:], labels[num_train_examples:]
+    train_data = th.tensor(data[:num_train_examples]).float()
+    train_labels = th.tensor(labels[:num_train_examples])
+    test_data = th.tensor(data[num_train_examples:]).float()
+    test_labels = th.tensor(labels[num_train_examples:])
+
+    return train_data, train_labels, test_data, test_labels
 
 
 if __name__ == '__main__':
